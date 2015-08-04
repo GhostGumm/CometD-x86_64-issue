0e0d07b1a2659ef462c2f28424f91d4156bdb444
Hakim <elbakk_a@outlook.com>
2015-08-04 15:18:28 +0200

    upload source for StackOverflow

---
+++ OsxIosFramework/DefinitiveOSxFramework/DefinitiveOSxFramework.h
@@ -0,0 +1,21 @@
+//
+//  DefinitiveOSxFramework.h
+//  DefinitiveOSxFramework
+//
+//  Created by Hakim on 30/07/15.
+//  Copyright (c) 2015 Hakim. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for DefinitiveOSxFramework.
+FOUNDATION_EXPORT double DefinitiveOSxFrameworkVersionNumber;
+
+//! Project version string for DefinitiveOSxFramework.
+FOUNDATION_EXPORT const unsigned char DefinitiveOSxFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DefinitiveOSxFramework/PublicHeader.h>
+        #import <DefinitiveOSxFramework/ZetaInit+Bridge.h>
+        #import <DefinitiveOSxFramework/libZeta.h>
+
+
